9f8ba10e34b376f5b1477671966747b1db76fc19
Thaiming <berthepd@gmail.com>
2021-07-21 17:23:46 +0900

    3rd day
    Middleware

--- src/server.js
+++ src/server.js
@@ -4,16 +4,31 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+    console.log(`PATH: ${req.path} METHOD: ${req.method} URL: ${req.url}`);
+    next();
+}
+
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if (url === "/protected") {
+        return res.send("<h1>Not Allowed</h1>")
+    }
+    console.log("Allowed");
+    next();
+}
+
 const handleHome = (req, res) => {
     return res.send("<h1>I still love you!!</h1>");
 };
 
-const handleLogin = (req, res) => {
-    return res.send("Login Here!");
+const handleProtected = (req, res) => {
+    return res.send("<h1>U A Special!</h1>")
 }
 
+app.use(logger, privateMiddleware);
 app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
     console.log(`Server listening on port http://localhost:${PORT} 🥰`);
